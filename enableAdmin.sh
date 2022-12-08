94ccff3e0a3a6b10d0e4b55169d49e2263e7a249
AlexanderC2333 <69483598+AlexanderC2333@users.noreply.github.com>
2022-12-07 20:00:07 -0500

    Added enableAdmin.sh

---
+++ enableAdmin.sh
@@ -0,0 +1,18 @@
+echo "Please enter user."
+
+read user
+
+usermod -aG %user adm
+usermod -aG %user dialout
+usermod -aG %user cdrom
+usermod -aG %user sudo
+usermod -aG %user audio
+usermod -aG %user video
+usermod -aG %user plugdev
+usermod -aG %user games
+usermod -aG %user users
+usermod -aG %user input
+usermod -aG %user netdev
+usermod -aG %user gpio
+usermod -aG %user i2c
+usermod -aG %user spi
